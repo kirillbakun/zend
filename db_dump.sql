5f956be67f8121268f9ca53365c9c996cfb31b1b
kirill.bakun <kirill.bakun@hiqo-solutions.com>
2014-07-24 20:02:49 +0300

    new dump

--- db_dump.sql
+++ db_dump.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2014-07-23 19:44:12
+Date: 2014-07-24 20:01:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,12 +27,11 @@ CREATE TABLE `article` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `article_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of article
 -- ----------------------------
-INSERT INTO `article` VALUES ('1', '1', '1', 'text_1');
 INSERT INTO `article` VALUES ('2', '1', null, 'text_2');
 INSERT INTO `article` VALUES ('3', '1', null, 'test artice');
 INSERT INTO `article` VALUES ('4', '3', '1', 'one more atricle');
@@ -40,6 +39,44 @@ INSERT INTO `article` VALUES ('6', null, null, 'efqefwfewe');
 INSERT INTO `article` VALUES ('7', '3', '1', '123123');
 INSERT INTO `article` VALUES ('8', null, '1', '76373737');
 INSERT INTO `article` VALUES ('10', '1', '1', 'text_1234');
+
+-- ----------------------------
+-- Table structure for `entity`
+-- ----------------------------
+DROP TABLE IF EXISTS `entity`;
+CREATE TABLE `entity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `is_active` tinyint(1) DEFAULT NULL,
+  `table` varchar(100) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `plural_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of entity
+-- ----------------------------
+INSERT INTO `entity` VALUES ('1', '1', 'article', 'article', 'articles');
+
+-- ----------------------------
+-- Table structure for `fields_list`
+-- ----------------------------
+DROP TABLE IF EXISTS `fields_list`;
+CREATE TABLE `fields_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` int(11) DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT NULL,
+  `position` tinyint(1) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  CONSTRAINT `fields_list_ibfk_1` FOREIGN KEY (`entity_id`) REFERENCES `entity` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of fields_list
+-- ----------------------------
+INSERT INTO `fields_list` VALUES ('2', '1', '1', '2', 'Text');
 
 -- ----------------------------
 -- Table structure for `user`
